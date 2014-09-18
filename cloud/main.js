28cbfe9757037a918c80607fd5142cadaacbbda1
Colin Ramsay <colinramsay@gmail.com>
2014-09-18 14:50:51 +0100

    boilerplate for sending SMS

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,24 @@
+var client = require('twilio')('ACc728a749974a87ed179b8a8af525965c', '1224720c600ba86ace438968cb887257');
+ 
+Parse.Cloud.define('sendSMS', function(request, response) {
+    client.sendSms({
+        to:'+447593117448', 
+        from: '+18559771780', 
+        body: 'fix030s0001n123456'
+      }, function(err, responseData) { 
+        if (err) {
+            console.log(err);
+            response.error();
+        } else { 
+            
+            console.log(responseData.from); 
+            console.log(responseData.body);
+            response.success();
+        }
+      }
+    );
+});
+
 Parse.Cloud.define('receiveSMS', function(request, response) {
     var raw = request.params.Body.split(',');
 
