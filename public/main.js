17f7cf228ae513e9ac601848f36fb9b9e3dbd57e
Colin Ramsay <colinramsay@gmail.com>
2014-10-01 11:10:17 +0100

    Only use the latest result from parse

--- public/main.js
+++ public/main.js
@@ -1,29 +1,42 @@
 Parse.initialize('nscyLFNtiKcHii5y3gly7g9EXERaljN41fQEQA40', 'S62ZcZ1aPbiPc44Sq1b9Jfh1ZLMUzwbw27zSoEly');
 
 var Location = Parse.Object.extend("location"),
-    query = new Parse.Query(Location),
-    mapOptions = {
-        zoom: 5,
-        center: new google.maps.LatLng(50.77815527465919, 7.03125),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    },
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    query = new Parse.Query(Location);
 
-query.find({
-    success: function(results) {
-        var coords = results.map(function(l) {
-            return new google.maps.LatLng(l.get('location').latitude, l.get('location').longitude);
+query.descending("createdAt").first({
+    success: function(coord) {
+
+        // Transform the Parse results into Google lat-longs
+        coord = new google.maps.LatLng(coord.get('location').latitude, coord.get('location').longitude);
+
+        // map config, centered on the coordinate
+        var mapOptions = {
+            zoom: 12,
+            center: coord,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+
+        // Popup window containing label
+        var infowindow = new google.maps.InfoWindow({
+            content: 'We\'re here!'
         });
 
-        var path = new google.maps.Polyline({
-            path: coords,
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
+        // Create the map from the map-canvas DOM element
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        // Create a pin to show the current coord
+        var marker = new google.maps.Marker({
+            position: coord,
+            map: map
         });
 
-        path.setMap(map);
+        // Wait a second then bounce the pin to draw attention.
+        setTimeout(function() {
+            marker.setAnimation(google.maps.Animation.DROP);
+
+            // Auto-pop open the info popup
+            infowindow.open(map, marker);
+        }, 1000);
     },
     error: function(error) {
         alert("Error: " + error.code + " " + error.message);
